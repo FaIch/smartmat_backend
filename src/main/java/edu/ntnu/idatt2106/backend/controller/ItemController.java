e45a71ababa70bd7473fc74e2c444e701ffdfeeb
charlohc <Charlottee.Christiansen@gmail.com>
2023-04-17 14:48:12 +0200

    Implemented methods for handling item functionality

--- src/main/java/edu/ntnu/idatt2106/backend/controller/ItemController.java
+++ src/main/java/edu/ntnu/idatt2106/backend/controller/ItemController.java
@@ -1,13 +1,55 @@
 package edu.ntnu.idatt2106.backend.controller;
 
+import edu.ntnu.idatt2106.backend.model.ItemEntity;
+import edu.ntnu.idatt2106.backend.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @EnableAutoConfiguration
 @CrossOrigin("*")
 @RequestMapping(value = "/api/items")
 public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    //Todo: create upload endpoint(??) when have user and know what parameters
+
+    @GetMapping("/list")
+    public List<ItemEntity> list(){
+        return itemService.getAllItems();
+    }
+
+    @GetMapping("/list/{id}")
+    public ResponseEntity<ItemEntity> getItemById(@PathVariable Long id){
+        ItemEntity item = itemService.getItem(id);
+        if(item == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }else{
+            return ResponseEntity.ok(item);
+        }
+    }
+
+  /*  @GetMapping("/list/me")
+    public List<ItemEntity> getItemsByUser(@AuthenticationPrincipal UserEntity user){
+        return itemService.getItemByUser(user);
+    }*/
+
+    @GetMapping("/list/category")
+    public List<ItemEntity> getItemsByCategory(@RequestParam("category") String category){
+        return itemService.getItemByCategory(category);
+    }
+
+  /*  @GetMapping("categories")
+    public List<Category> getCategories(){
+        return Arrays.stream(Category.values()).toList();
+    }
+*/
 }
--- src/main/java/edu/ntnu/idatt2106/backend/repository/ItemRepository.java
+++ src/main/java/edu/ntnu/idatt2106/backend/repository/ItemRepository.java
@@ -3,6 +3,8 @@ package edu.ntnu.idatt2106.backend.repository;
 import edu.ntnu.idatt2106.backend.model.ItemEntity;
 //import org.springframework.data.jpa.repository.JpaRepository;
 
-
-public class ItemRepository  /*extends JpaRepository<ItemEntity, Long>*/{
+/**
+ * Connectiong backend to database(?)
+ */
+public interface ItemRepository  /*extends JpaRepository<ItemEntity, Long>*/{
 }
