a171fcef92c4efa84a2a4363bd96232aa640d894
charlohc <Charlottee.Christiansen@gmail.com>
2023-04-18 10:33:05 +0200

    implemented more endpoint functionality for item

--- src/main/java/edu/ntnu/idatt2106/backend/repository/ItemRepository.java
+++ src/main/java/edu/ntnu/idatt2106/backend/repository/ItemRepository.java
@@ -1,10 +1,26 @@
 package edu.ntnu.idatt2106.backend.repository;
 
-import edu.ntnu.idatt2106.backend.model.ItemEntity;
+import edu.ntnu.idatt2106.backend.model.item.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * Connectiong backend to database(?)
  */
-public interface ItemRepository  extends JpaRepository<ItemEntity, Long> {
+public interface ItemRepository  extends JpaRepository<Item, Long> {
+    /**
+     * Method findByCategory
+     * @param category string category of the items which we want
+     * @return a list of items of the given category
+     */
+    List<Item> findByCategory(String category);
+
+  /*  *//**
+     * Method findAllByUser
+     * @param user entity of user which we want to find all items
+     * @return list of all items which specific user sells
+     *//*
+    List<ItemEntity> findAllByUser(User user);
+*/
 }
