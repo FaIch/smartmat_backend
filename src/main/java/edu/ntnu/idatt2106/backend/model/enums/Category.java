0abc9a0afee73c83434e94cf2a9fb09e3c9bcb53
charlohc <Charlottee.Christiansen@gmail.com>
2023-04-18 09:15:07 +0200

    Implemented more columns in item

---
+++ src/main/java/edu/ntnu/idatt2106/backend/model/enums/Category.java
@@ -0,0 +1,17 @@
+package edu.ntnu.idatt2106.backend.model.enums;
+
+public enum Category {
+    DRINKS,
+    BAKEDGOODS,
+    FISH,
+    FRUIT,
+    MEAT,
+    DAIRY,
+    EGG,
+    CHEESE,
+    CHICKEN,
+    PAALEGG,
+    CANDY,
+    VEGETABLES,
+
+}
