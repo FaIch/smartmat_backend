b39522061436bb2f95c57a812e8a241e47abfcc9
charlohc <Charlottee.Christiansen@gmail.com>
2023-04-18 09:33:07 +0200

    Fixed bug item classes

--- src/main/java/edu/ntnu/idatt2106/backend/service/ItemService.java
+++ src/main/java/edu/ntnu/idatt2106/backend/service/ItemService.java
@@ -1,22 +1,40 @@
 package edu.ntnu.idatt2106.backend.service;
 
 import edu.ntnu.idatt2106.backend.model.ItemEntity;
+import edu.ntnu.idatt2106.backend.repository.ItemRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface ItemService {
+
+@Service
+public class ItemService {
 
 
-    public List<ItemEntity> getAllItems();
-
-    public ItemEntity getItem(Long id);
+    private ItemRepository itemRepository;
 
 
-   // public List<ItemEntity> getItemByUser(UserEntity user);
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    //Todo: fix methods when connected backend to database
+    public List<ItemEntity> getAllItems() {
+        return null;
+    }
 
 
-    List<ItemEntity> getItemByCategory(String category);
+    public ItemEntity getItem(Long id) {
+        return null;
+    }
 
-    public void saveOrUpdate(ItemEntity itemEntity);
 
+    public List<ItemEntity> getItemByCategory(String category) {
+        return null;
+    }
+
+
+    public void saveOrUpdate(ItemEntity itemEntity) {
+
+    }
 }
