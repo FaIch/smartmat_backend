fe8ee49540707c96aff8fbf5f3da1b3eb769e1ba
charlohc <Charlottee.Christiansen@gmail.com>
2023-05-05 12:39:42 +0200

    removed AutoConfiguration param

--- src/main/java/edu/ntnu/idatt2106/backend/controller/RecipeController.java
+++ src/main/java/edu/ntnu/idatt2106/backend/controller/RecipeController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@EnableAutoConfiguration
 @CrossOrigin("*")
 @RequestMapping(value = "/recipe")
 public class RecipeController {
